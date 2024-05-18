0965f4aaa60a1b804d6c5bec955cc6100fb6aa95
jmDevstro <jmarquez.store@gmail.com>
2024-05-18 14:39:30 -0600

    Nueva vista para listar lamparas

--- Juan/views.py
+++ Juan/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Lampara
 
 # Create your views here.
+def lamparas(request):
+    if request.method == "GET":
+        lamparas = Lampara.objects.all()
+        return render(request, "lamparaslista.html", {
+            "lamparas": lamparas
+        })
