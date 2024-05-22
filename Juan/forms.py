0d0f65afec9efb9128ad1ceacf03621d77ac1666
jmDevstro <jmarquez.store@gmail.com>
2024-05-21 20:52:13 -0600

    Se eliminó forms.py

--- Juan/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import Lampara
-
-class LamparaForm(ModelForm):
-    class Meta:
-        model = Lampara
-        fields = ['nombre','precio','cantidad','fecha']
--- Juan/views.py
+++ Juan/views.py
@@ -1,49 +1,10 @@
-from django.shortcuts import render, redirect
-from django.conf import settings
+from rest_framework import viewsets, permissions
+from .serializers import ProjectSerializer
 from .models import Lampara
-from .forms import LamparaForm
 
-import requests
 
 # Create your views here.
-def lamparas(request):
-    if request.method == "GET":
-        lamparas = Lampara.objects.all()
-        return render(request, "lamparaslista.html", {
-            "lamparas": lamparas
-        })
-    
-    elif request.method == "POST":
-        if request.POST["_method"] == "DELETE":
-            print("Eliminar elemento")
-            res = requests.delete(settings.SITE_URL+request.POST["endpoint"])
-
-            print(res)
-            return redirect('listalamparas')
-        
-def lamparaNueva(request):
-    if request.method == "GET":
-        return render(request, "nuevalampara.html", {
-            "form": LamparaForm
-        })
-    
-    elif request.method == "POST":
-        res = requests.post(settings.SITE_URL+"/api/lampara/", data=request.POST)
-        return redirect('listalamparas')
-    
-def lamparaEditar(request):
-    if request.method == "POST" and "_method" in request.POST:
-        if request.POST["_method"] == "PUT":
-            print("Actualizando elemento...")
-            res = requests.put(settings.SITE_URL+request.POST["endpoint"], data=request.POST)
-            print(res.status_code)
-            print(res.content)
-            return redirect('listalamparas')
-
-    elif request.method == "POST":
-        res = requests.get(settings.SITE_URL+"/api/lampara/"+request.POST["lampara"])
-        #print(res.json())
-        return render(request, "editarlampara.html", {
-            "form": LamparaForm(res.json()),
-            "lampid": request.POST["lampara"]
-        })
+class ProjectViewSetLampara(viewsets.ModelViewSet):
+    queryset = Lampara.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = ProjectSerializer
