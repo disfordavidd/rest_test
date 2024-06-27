ab827a5067953b6f4089bc0f6f13761ce4b5c4e8
disfordavidd <disfordavidd@gmail.com>
2024-06-26 22:49:42 -0600

    bootstrap5

--- test_site/settings.py
+++ test_site/settings.py
@@ -43,7 +43,9 @@ INSTALLED_APPS = [
     'rest_framework',
     'David',
     'Juan',
+    'bootstrap5',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -135,7 +137,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+#STATICFILES_DIRS = [
+    #BASE_DIR / "static",
+#]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
